a569bba72ec8348105e944b12ddf366b87008b40
arturo-leiif <arturo.leiif@gmail.com>
2024-10-08 16:05:38 +1000

    Delete waypoint/build directory

--- waypoint/build/waypoint_commander/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/oliver/turtlebot3_ws/src/waypoint_commander/install/waypoint_commander'
